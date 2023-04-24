5b339f6c707b25a60a4b75912e45e00a00e6a3a7
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-04-25 00:17:37 +0200

    Model Category

---
+++ model/ModelCategory.php
@@ -0,0 +1,45 @@
+<?php
+/* c'est le DTO (Data Transfert Object) la
+représentation objet d’une table, c’est-à-dire
+l’objet métier. Il ne contient que des propriétés
+et des accesseurs correspondants. */
+
+require_once ("Model.php"); 
+
+class ModelCategory extends Model{
+//Même noms et ordre que dans la table utilisateur
+  private $id;
+  private $name_category ;
+
+
+  protected static $table = 'category';
+  protected static $primary = 'id';
+   
+  // ====== constructeur =======
+  public function __construct($name_category) {
+    if ($name_category != NULL) {
+      $this->name_category = $name_category ;
+    }
+  }
+  
+   
+  
+  // ==================== GETTERS ====================
+    public function getId() {
+         return $this->id;  
+    }
+    public function getName_category() {
+         return $this->name_category;  
+    }
+  // ==================== SETTERS ====================
+
+    public function setId($id) {
+         $this->id = $id;
+    }
+    public function setName_category($name_category) {
+         $this->name_category = $name_category;
+    }
+
+  
+}
+?>
