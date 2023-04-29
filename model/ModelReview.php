b6c23dab39c494e41de1b0dce1884f4ca39dada7
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-04-29 10:34:22 +0200

    Update ModelReview.php

--- model/ModelReview.php
+++ model/ModelReview.php
@@ -9,18 +9,30 @@ require_once ("Model.php");
 class ModelReview extends Model{
 //Même noms et ordre que dans la table utilisateur
   private $id;
-  private $name_category ;
+  private $description ;
+  private $id_user ;
+  private $id_produit ;
+  private $createdAt ;
 
 
-  protected static $table = 'category';
+
+  protected static $table = 'review';
   protected static $primary = 'id';
    
   // ====== constructeur =======
-  public function __construct($name_category) {
-    if ($name_category != NULL) {
-      $this->name_category = $name_category ;
+  public function __construct(
+    $description = NULL,
+    $id_user = NULL,
+    $id_produit = NULL,
+    $createdAt = NULL
+  ) {
+    if ($description != NULL && $id_user != NULL && $id_produit != NULL && $createdAt != NULL) {
+      $this->description = $description ;
+      $this->id_user = $id_user ;
+      $this->id_produit = $id_produit ;
+      $this->createdAt = $createdAt ;
     }
-  }
+  } 
   
    
   
@@ -28,18 +40,35 @@ class ModelReview extends Model{
     public function getId() {
          return $this->id;  
     }
-    public function getName_category() {
-         return $this->name_category;  
+    public function getDescription() {
+         return $this->description;  
+    }
+    public function getId_user() {
+         return $this->id_user;  
+    }
+    public function getId_produit() {
+         return $this->id_produit;  
+    }
+    public function getCreatedAt() {
+         return $this->createdAt;  
     }
   // ==================== SETTERS ====================
 
     public function setId($id) {
          $this->id = $id;
     }
-    public function setName_category($name_category) {
-         $this->name_category = $name_category;
+    public function setDescription($description) {
+         $this->description = $description;
     }
-
+    public function setId_user($id_user) {
+         $this->id_user = $id_user;
+    }
+    public function setId_produit($id_produit) {
+         $this->id_produit = $id_produit;
+    }
+    public function setCreatedAt($createdAt) {
+         $this->createdAt = $createdAt;
+    }
   
 }
 ?>
